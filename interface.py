328079a18e46e93742909f06485d83cc0b04b41c
Matheus Oliveira <114704235+greenShimada@users.noreply.github.com>
2023-04-16 17:49:51 -0300

    Imagem agora é 'croppada' para o tamanho minimo

--- interface.py
+++ interface.py
@@ -1,14 +1,12 @@
 from tkinter import *
-from tkinter import Tk, ttk
+from tkinter import Tk
 from PIL import Image, ImageTk
 import urllib.request
-from time import sleep
 import requests
 import json
-from rembg import remove
+
 
 # definindo as cores da pokedex
-
 vermelho = '#c73736'
 vermelho_escuro = '#8b2823'
 cinza_claro = '#e8ddc1'
@@ -30,44 +28,55 @@ janela.configure(background=vermelho_escuro)
 janela.resizable(width=FALSE,height=FALSE)
 
 # seções (frames)
+
+# FRAME DE CIMA (PARTE SUPERIOR DA POKEDEX ONDE TEM O INPUT)
 frameCima = Frame(janela, width=int(x), height=(int(y)*0.17), bg=vermelho, relief='raised', borderwidth=3)
 frameCima.grid(row=0, column=0)
-
+# FRAME DO MEIO (CORPO DA POKEDEX ONDE MOSTRA O POKEMON)
 frameMeio = Frame(janela, width=int(x), height=(int(y)*0.71), bg=vermelho_escuro, relief='flat')
 frameMeio.grid(row=1, column=0)
-
+# FRAME DE BAIXO (PARTE INFERIOR DA POKEDEX ONDE NÃO TEM NADA)
 frameBaixo = Frame(janela, width=int(x), height=(int(y)*0.12), bg=vermelho, relief='raised', borderwidth=2)
 frameBaixo.grid(row=2, column=0)
 
+# POSICIONANDO O "MONITOR" DA POKEDEX
 visor = Image.open('pokeW.jpg')
 visor = visor.resize((400, 397))
 visor = ImageTk.PhotoImage(visor)
 teste = Label(frameMeio, image=visor)
 teste.place(y=0, x=0)
 
-def tirarFundoPNG(caminho, saida):
-    input = Image.open(caminho)
-    output = remove(input) #amo python
-    output.save(saida)
+
+def diminuirImagem(img):
+    with Image.open(img) as pokemon:
+        img_pretobranco = pokemon.convert('L')            # Converte a imagem para escalas de cinza
+        bbox = img_pretobranco.getbbox()                  # Acha sozinho o menor tamanho possível sem o fundo (amo python)
+        pokemon = pokemon.crop(bbox)                      # bbox retorna uma tupla com as coordenadas da borda superior esquerda e inferior direita, logo ele sozinho serve de parametro para o crop
+        pokemon.save(img, format='PNG')                   # salva a imagem com o tamanho nova por cima da antiga
 
 def procurar():
-    global poke_img
-    # Obtendo a palavra
-    palavra = e_palavra.get()
-    print(palavra)
-    api_link = f'https://pokeapi.co/api/v2/pokemon/{palavra}'
+    global poke_img                                                 # SE NÃO FOR GLOBAL, A IMAGEM NÃO É RECONHECIDA APÓS O FIM DA FUNÇÃO
+    palavra = e_palavra.get()                                       # RECEBE O QUE FOI DIGITADO NO INPUT
+    api_link = f'https://pokeapi.co/api/v2/pokemon/{palavra}'       # ACESSA A API COM O NOME DO POKEMON DIGITADO
     r = requests.get(api_link)
-    dados = json.loads(r.text)
+    if r.status_code != 200:
+        api_link = f'https://pokeapi.co/api/v2/pokemon/bulbasaur'   # SE DIGITAR ERRADO VOLTA O BULBASAURO
+        r = requests.get(api_link)
+    dados = json.loads(r.text)                                      # TRANSFORMA O JSON DA API EM UM OBJETO PYTHON
 
-    # imagem
     img = dados['sprites']['front_default']
-    poke_img = urllib.request.urlretrieve(img, 'poke.png')
-    tirarFundoPNG('poke.png','poke.png')
-    poke_img = Image.open(poke_img[0])
-    poke_img = poke_img.resize((300, 300))
+    poke_img = urllib.request.urlretrieve(img, 'poke.png')          # BAIXA A IMAGEM PELA URL E SALVA COMO 'poke.png'
+    diminuirImagem('poke.png')                                      # FUNÇÃO QUE RETIRA O FUNDO PRETO E DEIXA O MENOR POSSÍVEL
+
+    poke_img = Image.open(poke_img[0])                              # PRECISA DO [0] PORQUE É UMA TUPLA ONDE O SEGUNDO ITEM É O CABEÇALHO DO RETORNO HTTP
+    altura, largura = poke_img.size
+    altura_nova = altura*3
+    largura_nova = largura*3
+    poke_img = poke_img.resize((altura_nova, largura_nova))
     poke_img = ImageTk.PhotoImage(poke_img)
-    poke_img_ready = Label(frameMeio, image=poke_img, width=299, height=299, bg='#ffffff')
-    poke_img_ready.place(x=(int(x)/2 - 150), y=20)
+    poke_img_ready = Label(frameMeio, image=poke_img, bg='#ffffff')
+
+    poke_img_ready.place(x=( (int(x)/2) - (largura_nova/2)), y = 20 + (altura_nova//2))        # MAIOR POKEMON OCUPA 80 PIXEIS EM Y
 
     abilities = []
     for ability in dados['abilities']:
@@ -76,13 +85,15 @@ def procurar():
     tk_hab = Label(frameMeio, text=habilidades, font='Ivy 9', bg='#ffffff')
     tk_hab.place(x=30, y=300)
 
-# Personalizando Frame do Meio
-
+# CRIANDO A LABEL COM O TEXTO
 l_palavra = Label(frameCima, text='Digite o nome de um pokemon', height=1, anchor=NW, font='Ivy 11', bg=vermelho, fg=cinza_claro)
 l_palavra.place(x=7, y=15)
+
+# CRIANDO  O INPUT PARA DIGITAR O NOME DO POKEMON
 e_palavra = Entry(frameCima, width=18, font=('Ivy 14 '), justify='center',relief="solid")
 e_palavra.place(x=10, y=41)
 
+# CRIANDO O BOTÃO E ASSOCIANDO A ELE A FUNÇÃO DE PROCURAR O POKEMON NA API
 b_procurar = Button(frameCima,command=procurar, compound=CENTER,width=10, text='Procurar' ,bg=azul_claro,font=('Ivy 11'), overrelief=RIDGE,)
 b_procurar.place(x=260, y=40)
 
